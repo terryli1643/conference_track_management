8e25bddf110d65ac971aa95ab0f0c442ca4cf64d
lizhen1643 <terry_lee@yeah.net>
2020-12-27 17:12:21 +0800

    update

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"os"
 	"strconv"
 	"strings"
+	"time"
 )
+
+var logger = log.New(os.Stderr, "", log.LstdFlags)
+
+// var logger = log.New(ioutil.Discard, "", log.LstdFlags)
 
 var talkList = TalkList{
 	data: list.New(),
@@ -25,107 +31,60 @@ func (t TalkList) String() (str string) {
 }
 
 type Talk struct {
-	title string
-	time  int
+	Title string
+	Time  int
 }
 
 func (t Talk) String() string {
-	return fmt.Sprint(t.time) + " "
-}
-
-type Session struct {
-	talk   *list.List
-	MaxLen int
-	MinLen int
-	Done   bool
-}
-
-func (s Session) String() (str string) {
-	for e := s.talk.Front(); e != nil; e = e.Next() {
-		str += e.Value.(Talk).String()
-	}
-	return str
+	return fmt.Sprint(t.Time) + " "
 }
 
 type Track struct {
-	morning   Session
-	afternoon Session
+	morning   *Session
+	afternoon *Session
 	nextDay   *Track
-}
-
-func init() {
-	initTalkList(testInput)
-}
-
-func main() {
-	track, err := plan(talkList)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	fmt.Printf("%+v", track)
-}
-func (s Session) set(t Talk) (err error) {
-	//如果插入的talk总时间大于session的最大时间，则先从session中移除比当前talk时间大的talk, 再插入
-	if s.getTotalTime()+t.time > s.MaxLen {
-		for e := s.talk.Back(); e != nil; e = e.Prev() {
-			if e.Value.(Talk).time > t.time {
-				s.talk.Remove(e)
-				putTalkToTalkList(talkList, e.Value.(Talk))
-				fmt.Printf("remove %d from %+v, talks list is : %+v\n", e.Value.(Talk).time, s, talkList)
-			}
-		}
-	}
-	if s.getTotalTime()+t.time > s.MaxLen {
-		err = errors.New("error")
-		fmt.Printf("session is %v, talks list is : %+v\n", s, talkList)
-		return err
-	}
-	s.talk.PushBack(t)
-	//如果刚好填满session，设置session为已完成
-	if s.getTotalTime()+t.time == s.MaxLen {
-		s.Done = true
-	}
-	return nil
-}
-
-func (s Session) getTotalTime() (total int) {
-	if s.talk.Len() == 0 {
-		return 0
-	}
-	for e := s.talk.Front(); e != nil; e = e.Next() {
-		total += e.Value.(Talk).time
-	}
-	return
 }
 
 func newTrack() *Track {
 	return &Track{
-		morning: Session{
+		morning: &Session{
 			MaxLen: 180,
 			MinLen: 180,
 			talk:   list.New(),
 			Done:   false,
+			Type:   "morning",
 		},
-		afternoon: Session{
+		afternoon: &Session{
 			MaxLen: 240,
 			MinLen: 180,
 			talk:   list.New(),
 			Done:   false,
+			Type:   "afternoon",
 		},
 	}
 }
 
-func (t Track) set(talk Talk) (err error) {
+func (t *Track) String() (str string) {
+	str = fmt.Sprintf("morning : %+v, afternoon : %+v, [%+v]", t.morning, t.afternoon, t.nextDay)
+	return
+}
+
+func (t *Track) set(talk Talk) (err error) {
 	if t.morning.Done == false {
 		err = t.morning.set(talk)
 	} else if t.afternoon.Done == false {
 		err = t.afternoon.set(talk)
 	} else {
-		t.nextDay = newTrack()
+		if t.nextDay == nil {
+			t.nextDay = newTrack()
+		}
 		err = t.nextDay.set(talk)
 	}
 	return err
+}
+
+func init() {
+	initTalkList(testInput)
 }
 
 func initTalkList(input []string) {
@@ -135,56 +94,156 @@ func initTalkList(input []string) {
 		timeStr := v[n+1:]
 
 		if timeStr == "lightning" {
-			putTalkToTalkList(talkList, Talk{
-				title: title,
-				time:  5,
+			putToTalkList(talkList, Talk{
+				Title: title,
+				Time:  5,
 			})
 		} else if strings.HasSuffix(timeStr, "min") {
 			t := timeStr[:len(timeStr)-3]
 			time, err := strconv.Atoi(t)
 			if err != nil {
-				log.Fatal(err)
+				logger.Fatal(err)
 			}
-			putTalkToTalkList(talkList, Talk{
-				title: title,
-				time:  time,
+			putToTalkList(talkList, Talk{
+				Title: title,
+				Time:  time,
 			})
 		}
 	}
-	fmt.Printf("%+v\n", talkList)
-	fmt.Println("====================================================================")
+	logger.Printf("%+v\n", talkList)
+	logger.Println("====================================================================")
 	return
 }
 
-func putTalkToTalkList(l TalkList, t Talk) {
+func main() {
+	track, err := plan(talkList)
+	if err != nil {
+		logger.Fatal(err)
+		return
+	}
+	logger.Printf("result : %+v", track)
+
+	Print(track, 1)
+}
+
+func Print(track *Track, counter int) {
+	fmt.Printf("Track %d\n", counter)
+	beginTime := "9:00AM"
+	for e := track.morning.talk.Front(); e != nil; e = e.Next() {
+		fmt.Printf("%s %s %dmin\n", beginTime, e.Value.(Talk).Title, e.Value.(Talk).Time)
+		beginTime = getClock(beginTime, e.Value.(Talk).Time)
+	}
+
+	fmt.Println("12:00PM Lunch")
+
+	beginTime = "1:00PM"
+	for e := track.afternoon.talk.Front(); e != nil; e = e.Next() {
+		fmt.Printf("%s %s %dmin\n", beginTime, e.Value.(Talk).Title, e.Value.(Talk).Time)
+		beginTime = getClock(beginTime, e.Value.(Talk).Time)
+	}
+
+	if track.afternoon.getTotalTime() < 180 {
+		beginTime = "4:00PM"
+	}
+	fmt.Printf("%s Networking Event\n", beginTime)
+
+	if track.nextDay != nil {
+		Print(track.nextDay, counter+1)
+	}
+}
+
+type Session struct {
+	talk   *list.List
+	MaxLen int
+	MinLen int
+	Done   bool
+	Type   string
+}
+
+func (s Session) String() (str string) {
+	for e := s.talk.Front(); e != nil; e = e.Next() {
+		str += e.Value.(Talk).String()
+	}
+	return str
+}
+
+func (s *Session) set(t Talk) (err error) {
+	//如果插入的talk总时间大于session的最大时间，则先从session中移除比当前talk时间大的talk, 再插入
+	if s.getTotalTime()+t.Time > s.MaxLen {
+		for e := s.talk.Back(); e != nil; e = e.Prev() {
+			if (e.Value.(Talk).Time - t.Time) > (s.MaxLen - s.getTotalTime()) {
+				s.talk.Remove(e)
+				putToTalkList(talkList, e.Value.(Talk))
+				logger.Printf("remove %d from %s session %+v, talks list is : %+v\n", e.Value.(Talk).Time, s.Type, s, talkList)
+			}
+		}
+	}
+	if s.getTotalTime()+t.Time > s.MaxLen {
+		err = errors.New("error")
+		logger.Printf("errro :: session is %v, talks list is : %+v\n", s, talkList)
+		s.Done = true
+	} else {
+		s.talk.PushBack(t)
+		logger.Printf("put %+v to session : %v, talks list is : %+v\n", t, s, talkList)
+		if s.getTotalTime() == s.MaxLen {
+			s.Done = true
+		}
+	}
+	return err
+}
+
+func (s Session) getTotalTime() (total int) {
+	if s.talk.Len() == 0 {
+		return 0
+	}
+	for e := s.talk.Front(); e != nil; e = e.Next() {
+		total += e.Value.(Talk).Time
+	}
+	logger.Printf("total Time is %d \n", total)
+	return
+}
+
+func putToTalkList(l TalkList, t Talk) {
 	if l.data.Len() == 0 {
 		l.data.PushFront(t)
 		return
 	}
 
 	for e := l.data.Front(); e != nil; e = e.Next() {
-		if t.time >= e.Value.(Talk).time {
+		if t.Time >= e.Value.(Talk).Time {
 			l.data.InsertBefore(t, e)
 			return
 		}
 	}
 	l.data.PushBack(t)
-	fmt.Printf("push %v to talk list", t)
+	logger.Printf("push %v to talk list \n", t)
 }
 
-func plan(talks TalkList) (track *Track, err error) {
+func plan(talkList TalkList) (track *Track, err error) {
 	track = newTrack()
-	for e := talks.data.Front(); e != nil; e = e.Next() {
-		err = track.set(e.Value.(Talk))
+	for talkList.data.Len() > 0 {
+		var next *list.Element
+		for e := talkList.data.Front(); e != nil; e = next {
+			next = e.Next()
+			err = track.set(e.Value.(Talk))
+			if err != nil {
+				break
+			}
+			talkList.data.Remove(e)
+			logger.Printf("remove %+v from talk list : %+v \n", e.Value.(Talk), talkList)
+		}
 		if err != nil {
-			break
+			logger.Println(err)
+			logger.Printf("talks list is : %+v\n", talkList)
 		}
 	}
-	if err != nil {
-		// return plan(talks)
-		return nil, err
-	}
-	return nil, nil
+	return
+}
+
+func getClock(begin string, duration int) string {
+	t, _ := time.Parse("3:04PM", begin)
+	t = t.Add(time.Minute * time.Duration(duration))
+	return t.Format("3:04PM")
 }
 
 var testInput = []string{
@@ -194,7 +253,6 @@ var testInput = []string{
 	"Ruby Errors from Mismatched Gem Versions 45min",
 	"Common Ruby Errors 45min",
 	"Rails for Python Developers lightning",
-	"Communicating Over Distance 60min",
 	"Communicating Over Distance 60min",
 	"Accounting-Driven Development 45min",
 	"Woah 30min",
@@ -208,4 +266,64 @@ var testInput = []string{
 	"Ruby on Rails Legacy App Maintenance 60min",
 	"A World Without HackerNews 30min",
 	"User Interface CSS in Rails Apps 30min",
+}
+
+var testInput1 = []string{
+	"spend 75min",
+	"spend 75min",
+	"spend 69min",
+	"spend 69min",
+	"spend 68min",
+	"spend 60min",
+	"spend 59min",
+	"spend 57min",
+	"spend 48min",
+	"spend 48min",
+	"spend 43min",
+	"spend 43min",
+	"spend 40min",
+	"spend 40min",
+	"spend 39min",
+	"spend 38min",
+	"spend 37min",
+	"spend 37min",
+	"spend 36min",
+	"spend 36min",
+	"spend 34min",
+	"spend 33min",
+	"spend 33min",
+	"spend 31min",
+	"spend 31min",
+	"spend 31min",
+	"spend 31min",
+	"spend 31min",
+	"spend 31min",
+	"spend 31min",
+	"spend 31min",
+	"spend 31min",
+	"spend 30min",
+	"spend 30min",
+	"spend 30min",
+	"spend 30min",
+	"spend 30min",
+	"spend 30min",
+	"spend 30min",
+	"spend 30min",
+	"spend 30min",
+	"spend 30min",
+	"spend 30min",
+	"spend 30min",
+	"spend 30min",
+	"spend 30min",
+	"spend 27min",
+	"spend 27min",
+	"spend 27min",
+	"spend 27min",
+	"spend 27min",
+	"spend 27min",
+	"spend 27min",
+	"spend 55min",
+	"spend 55min",
+	"spend 55min",
+	"spend 55min",
 }
